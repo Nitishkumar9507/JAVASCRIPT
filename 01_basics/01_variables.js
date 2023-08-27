d4e6d292bf04431fbc6f6431da9954180c4890ea
Nitish kumar <138359899+Nitishkumar9507@users.noreply.github.com>
2023-08-27 07:16:02 +0000

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 9507;
+let email = "nitish@gmail.com";
+var password = "888888";
+accountCity = "jaipur"; // not prefered to use like this
+let myname; //this variable will be declared as undefined:
+
+// accountId=2; //it cant be redeclared as it is a const 
+
+email ="abcd@gmil.com";
+password="8757";
+accountCity="darbhanga";
+console.log(accountId);
+console.table([accountId,email,password,accountCity])
+
+/* var was used earlier now it is suggested to use const and let 
+prefer not to use var because of issue in block scope and functional scope
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+"use scrict" // treat all js code as newer version js
+
+// alert("hello Duniya") //can be used on browser dont run on node js
+
+/* Number(76), BigInt(8769789684876876486874), String("this is a string"), Boolean(True/False), Null(emty value/null), Symbol(used to uniquely identify the data types), Undefined(Not declared) */
+
+//object
+
+let nitish="my name is nitish"
+console.log(typeof(nitish)) //string
+let age =19;
+console.log(age)
+console.log(typeof(age)); //number
+console.log(typeof null); //object remember this
+console.log(typeof undefined); //undefined
