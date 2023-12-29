cee61a84674de5d3f50d7523ff59c2426d5cab54
Nitish kumar <138359899+Nitishkumar9507@users.noreply.github.com>
2023-12-29 18:19:56 +0530

    Update 01_variables.js
    note

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -10,7 +10,7 @@ email ="abcd@gmil.com";
 password="8757";
 accountCity="darbhanga";
 console.log(accountId);
-console.table([accountId,email,password,accountCity])
+console.table([accountId,email,password,accountCity]) // table is used to print the value of multiple variables in the array at a time
 
 /* var was used earlier now it is suggested to use const and let 
 prefer not to use var because of issue in block scope and functional scope
